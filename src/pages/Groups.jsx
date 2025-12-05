17608f6b5f4ca5652cd1950ef55927e5db865f21
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-12-04 20:50:27 -0600

    feature: base form complete

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+import { createNewGroup } from "../api/groups";
+
 export default function Groups() {
-  return <h1>Groups Page</h1>;
+  const { token } = useAuth();
+  const [error, setError] = useState(null);
+
+  const handleGroupFormSubmission = async () => {
+    try {
+      await createNewGroup(token, "new group name", "some description");
+    } catch (err) {
+      console.error(err.message);
+      setError(err.message);
+    }
+  };
+  console.log(token);
+
+  return (
+    <form className="auth-form" onSubmit={handleGroupFormSubmission}>
+      <label>Group name:</label>
+      <input type="text" name="group_name" required />
+      <label>Description:</label>
+      <input type="text" name="description" />
+      <button type="submit" className="auth-btn">
+        Submit
+      </button>
+      {error && <p className="error-text">{error}</p>}
+    </form>
+  );
 }
