f96093fd9e3c3cbc506a5c445472aabf33edfb09
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-12-07 14:03:20 -0600

    feature: Add button connection to navigate to groups/new on new group button click

--- src/pages/GroupsList.jsx
+++ src/pages/GroupsList.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router";
+
 export default function GroupsList() {
+  const navigate = useNavigate();
+
+  const handleNewGroupButton = () => {
+    navigate("new");
+  };
+
   return (
     <div>
+      <button className="new-group-btn" onClick={handleNewGroupButton}>
+        Create New Group
+      </button>
       <p>List of all groups...</p>
-      {/* button will be created to allow users to navigate to the create group form */}
     </div>
   );
 }
